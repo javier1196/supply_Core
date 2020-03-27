f02d8563944ce59753e005ebc218ec61cb757b5b
Javier Ibarra <javieremiliano1996@gmail.com>
2020-03-26 21:38:42 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='supply_core',
+      version='0.1',
+      description='Package with the uses cases',
+      url='',
+      author='TechTeam',
+      author_email='javieremiliano1996@gmail.com',
+      license='',
+      packages=find_packages(),
+      zip_safe=False)
---
+++ supply_core/use_cases/users.py
@@ -0,0 +1,32 @@
+class Users(object):
+
+    def _init_(self, storage_bridge):
+        self.storage_adapter = storage_bridge
+
+    def list(self):
+        try:
+            value = self.storage_adapter.list()
+            return value
+        except:
+            return "There is a problem"
+
+    def new(self, company):
+        try:
+            value = self.storage_adapter.new(company)
+            return value
+        except Exception:
+            return "There is a problem"
+
+    def delete(self, id):
+        try:
+            value = self.storage_adapter.delete(id)
+            return value
+        except Exception:
+            return "There is a problem"
+
+    def update(self, id, department):
+        try:
+            value = self.storage_adapter.update(id, department)
+            return value
+        except Exception:
+            return "There is a problem"
